b887d53837004a12c09f5ab1b47ed5af0fc57569
Xinlei Xu <38530926+Trojblue@users.noreply.github.com>
2022-12-18 18:26:48 -0500

    done: basic analytics to csv

--- mypixivpy.py
+++ mypixivpy.py
@@ -1,41 +1,49 @@
 import os
-
 from pixivpy3 import *
 from typing import *
-from datetime import datetime
 import time
 import csv
 from datetime import datetime, timezone
 
-user_ada = 88213414
-user_yada = 14626152
+from tqdm import tqdm
 
 
-def get_recover_token_ada():
+
+
+def get_recover_token(user:str="yada"):
     """实际获取token:
     https://gist.github.com/upbit/6edda27cb1644e94183291109b8a5fde
     """
-    with open("private/recover_token_ada.txt") as fd:
-        line = fd.readline()
-        return line
-
+    if user.lower() == "yada":
+        with open("private/recover_token_yada.txt") as fd:
+            line = fd.readline()
+            return line
+    elif user.lower() == "ada":
+        with open("private/recover_token_ada.txt") as fd:
+            line = fd.readline()
+            return line
 
 def get_time_string():
     return datetime.now().strftime("%y.%m.%d_%H%M")
 
-def get_user_illusts(api: AppPixivAPI, user: int) -> List[Dict]:
+def get_user_illusts(api: AppPixivAPI, user_tup: Tuple) -> List[Dict]:
     """返回pixiv id=<user>的所有illust json
     """
+    user=user_tup[0]
+
+
     all_illusts = []
 
     json_result = api.user_illusts(user)
     all_illusts += json_result['illusts']
 
+    pbar = tqdm(desc="collecting info for user %s: "%user)
     while json_result["next_url"]:
         time.sleep(0.5)
         next_qs = api.parse_qs(json_result.next_url)
         json_result = api.user_illusts(**next_qs)
         all_illusts += json_result['illusts']
+        pbar.update(30)
 
     return all_illusts
 
@@ -54,14 +62,16 @@ def get_hourly_time_difference(past_date, current_date):
     return difference_in_hours
 
 
-def illusts_to_csv_single_day(art_list, file_prefix):
+def illusts_to_csv_single_day(art_list:List, user_tup:Tuple):
     """
     :param art_list: 获取到的illusts
     :return: csv文件
     """
+    file_prefix = user_tup[1]  # "name"
+
     # Get the current datetime with the timezone set to the past_date timezone
     current_date = datetime.now(timezone.utc)
-    filename= file_prefix + get_time_string + '.csv'
+    filename= file_prefix + "_"+ get_time_string() + '.csv'
     out_dir = "./private/csv"
     out_path = os.path.join(out_dir, filename)
 
@@ -77,61 +87,33 @@ def illusts_to_csv_single_day(art_list, file_prefix):
             current_date = current_date.astimezone(post_date.tzinfo)
             pasted_hours = (current_date - post_date).total_seconds() / 3600
 
-            # pasted_hours = get_hourly_time_difference(c_date, current_date)
             bm_per_hour = '%.2f' % (d['total_bookmarks'] / pasted_hours)  # 每小时书签量
             view_per_hour = '%.2f' % (d['total_view'] / pasted_hours)
 
             book_view_rate = '%.2f' % (d['total_bookmarks']/ d['total_view'])
             view_book_rate = '%.2f' % (d['total_view'] / d['total_bookmarks'])
 
-            # EXCEL生成URL: =HYPERLINK("http://www.pixiv.net/artworks/"&B5)
+            # EXCEL生成URL列: =HYPERLINK("http://www.pixiv.net/artworks/"&B2)
 
             writer.writerow(
                 [c_date, d['id'], d['title'], d['total_view'], d['total_bookmarks'], d['width'], d['height'],
                  bm_per_hour, view_per_hour, book_view_rate, view_book_rate])
 
 
-def get_info(api: AppPixivAPI, user: int):
-    ada_details = api.user_detail(user)
-    usr_following = api.user_following(user)
-    usr_followers = api.user_follower(user)
-
-    print("D")
-
-
-def demo():
+def do_stats(user_tup: Tuple):
+    """工厂模式; 用user string对应实际方案
+    """
     api = AppPixivAPI()
-    token = get_recover_token_ada()
+    token = get_recover_token()
     api.auth(refresh_token=token)  # ada
 
-    i_liust = get_user_illusts(api, user_yada)
-    illusts_to_csv_single_day(i_liust)
-
-    # # get origin url
-    # json_result = api.illust_detail(59580629)
-    # illust = json_result.illust
-    # print(">>> origin url: %s" % illust.image_urls['large'])
-    #
-    # # get ranking: 1-30
-    # # mode: [day, week, month, day_male, day_female, week_original, week_rookie, day_manga]
-    # json_result = api.illust_ranking('day')
-    # for illust in json_result.illusts:
-    #     print(" p1 [%s] %s" % (illust.title, illust.image_urls.medium))
-    #
-    # # next page: 31-60
-    # next_qs = api.parse_qs(json_result.next_url)
-    # json_result = api.illust_ranking(**next_qs)
-    # for illust in json_result.illusts:
-    #     print(" p2 [%s] %s" % (illust.title, illust.image_urls.medium))
-    #
-    # # get all page:
-    # next_qs = {"mode": "day"}
-    # while next_qs:
-    #     json_result = api.illust_ranking(**next_qs)
-    #     for illust in json_result.illusts:
-    #         print("[%s] %s" % (illust.title, illust.image_urls.medium))
-    #     next_qs = api.parse_qs(json_result.next_url)
+    i_liust = get_user_illusts(api, user_tup)
+    illusts_to_csv_single_day(i_liust, user_tup)
 
 
 if __name__ == '__main__':
-    demo()
+    user_ada = (88213414, "ada")
+    user_yada = (14626152, "yada")
+    user_poi = (19755093, "poi")
+    # do_stats("yada")
+    do_stats(user_poi)
